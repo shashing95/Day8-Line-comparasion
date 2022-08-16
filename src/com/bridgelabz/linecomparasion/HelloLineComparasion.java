499fa25e65d845d6a04e44625597502b7d23e30c
shashi <shashidharngg@gmail.com>
2022-08-16 22:27:12 +0530

    added:welcome to line comparasion

---
+++ src/com/bridgelabz/linecomparasion/HelloLineComparasion.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linecomparasion;
+
+public class HelloLineComparasion {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program");
+		}
+
+	}
+
+
